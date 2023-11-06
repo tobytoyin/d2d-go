f1abfa92dd5f97f99721e4edaeb297fb584c60ba
Toby <40096033+tobytoyin@users.noreply.github.com>
2023-11-06 16:02:59 +0000

    add json file handling

--- api/source_to_document.go
+++ api/source_to_document.go
@@ -3,7 +3,31 @@ package api
 import (
 	"encoding/json"
 	"log"
+	"os"
 )
+
+type JSONSpecHandler struct{}
+
+// takes in the string content of JSON and parse into TaskSpec
+func (h JSONSpecHandler) JsonStringToTaskSpec(content string) TaskSpec {
+	spec := new(TaskSpec)
+	err := json.Unmarshal([]byte(content), spec)
+	if err != nil {
+		log.Fatal("Error during Unmarshal(): ", err)
+	}
+	return *spec
+}
+
+// takes in the JSON file and parse into TaskSpec
+func (h JSONSpecHandler) JsonFileToTaskSpec(path string) TaskSpec {
+	content, err := os.ReadFile(path)
+	if err != nil {
+		log.Fatalf("%s is not a valid JSON path", path)
+	}
+
+	taskSpec := h.JsonStringToTaskSpec(string(content))
+	return taskSpec
+}
 
 type AdapterSpec struct {
 	Adapter string
@@ -13,25 +37,3 @@ type AdapterSpec struct {
 type TaskSpec struct {
 	Spec map[string]AdapterSpec
 }
-
-type JSONSpecHandler struct{}
-
-func (h JSONSpecHandler) PayloadToStruct(content string) TaskSpec {
-	spec := new(TaskSpec)
-	err := json.Unmarshal([]byte(content), spec)
-	if err != nil {
-		log.Fatal("Error during Unmarshal(): ", err)
-	}
-	return *spec
-}
-
-// func (h JSONSpecHandler) LoadJson(path string) {
-// 	content, err := io.ReadFile(path)
-
-// 	if err != nil {
-// 		log.Fatal("Error when opening file: ", err)
-// 	}
-
-// 	// parse json
-// 	json.Unmarshal(content)
-// }
--- test/api/task_spec_handler.go
+++ test/api/task_spec_handler.go
@@ -16,7 +16,7 @@ func TestJsonToStructSingleNoOptions(t *testing.T) {
 	}
 	`
 	jsonHandler := new(api.JSONSpecHandler)
-	res := jsonHandler.PayloadToStruct(samplePayload)
+	res := jsonHandler.JsonStringToTaskSpec(samplePayload)
 
 	if res.Spec == nil {
 		t.Fatalf("spec is incorrect")
@@ -46,7 +46,7 @@ func TestJsonToStructSingleWithOptions(t *testing.T) {
 	`
 
 	jsonHandler := new(api.JSONSpecHandler)
-	res := jsonHandler.PayloadToStruct(samplePayload)
+	res := jsonHandler.JsonStringToTaskSpec(samplePayload)
 
 	// access and test the "options" key
 	if res.Spec["summary"].Options["arg1"] != "value1" {
@@ -73,7 +73,7 @@ func TestJsonToStructMixture(t *testing.T) {
 	}
 	`
 	jsonHandler := new(api.JSONSpecHandler)
-	res := jsonHandler.PayloadToStruct(samplePayload)
+	res := jsonHandler.JsonStringToTaskSpec(samplePayload)
 
 	if res.Spec["summary"].Adapter != "mock" {
 		t.Fatalf("summary key isn't loading correctly")
@@ -95,4 +95,32 @@ func TestJsonToStructMixture(t *testing.T) {
 		t.Fatalf("metadata options elements isn't loading correctly")
 	}
 
+}
+
+func TestJsonFileToTaskSpec(t *testing.T) {
+	path := "../../examples/task_spec.json"
+
+	jsonHandler := new(api.JSONSpecHandler)
+	res := jsonHandler.JsonFileToTaskSpec(path)
+
+	// same tests as TestJsonToStructMixture
+	if res.Spec["summary"].Adapter != "mock" {
+		t.Fatalf("summary key isn't loading correctly")
+	}
+
+	if res.Spec["summary"].Options != nil {
+		t.Fatalf("summary options isn't loading correctly")
+	}
+
+	if res.Spec["metadata"].Adapter != "mock" {
+		t.Fatalf("metadata key isn't loading correctly")
+	}
+
+	if res.Spec["metadata"].Options == nil {
+		t.Fatalf("metadata options isn't loading correctly")
+	}
+
+	if res.Spec["metadata"].Options["arg1"] != "value1" {
+		t.Fatalf("metadata options elements isn't loading correctly")
+	}
 }
